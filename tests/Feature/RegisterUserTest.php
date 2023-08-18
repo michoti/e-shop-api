61cd78c97c94cce13b7dd3539070251079055ce6
shukeel <triggaillest@gmail.com>
2023-08-18 19:56:44 +0300

    + feature tests

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -2,11 +2,35 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Arr;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\Validations\RegisterValidation as ValidationTest;
+use Tests\Validations\RegisterValidation;
 
 class RegisterUserTest extends TestCase
 {
-    // use RegisterValidation;
+    use RegisterValidation;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutExceptionHandling();
+    }
+
+    /** @test */
+    public function a_user_can_register()
+    {
+        $res = $this->postJson(route('auth.register'), $this->data, $this->header);
+
+        $res->assertCreated()
+            ->assertJson(
+                fn (AssertableJson $json) =>
+                $json->hasAll(['code', 'message'])
+            );
+
+        $this->assertDatabaseCount('users', 1)
+            ->assertDatabaseHas('users', Arr::except($this->data, ['password', 'password_confirmation']));
+    }
 }
